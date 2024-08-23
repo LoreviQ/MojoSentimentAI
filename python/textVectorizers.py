cca598cb49af07a72dae3f8a1ead58d7d231ba92
LoreviQ <lorevi@protonmail.com>
2024-08-23 14:00:19 +0100

    Refactored min_df to modify vocabulary_ not vector

--- python/textVectorizers.py
+++ python/textVectorizers.py
@@ -10,10 +10,10 @@ class CustomCountVectorizer:
     Custom implementation of the CountVectorizer class.
     """
 
-    def __init__(self, min_df=1):
+    def __init__(self, min_df=1, ngram_range=(1, 1)):
         self.vocabulary_ = {}
-        self.vocab_index_ = 0
         self.min_df = min_df
+        self.ngram_range = ngram_range
 
     def fit(self, documents):
         """
@@ -21,20 +21,28 @@ class CustomCountVectorizer:
         """
         for document in documents:
             for word in document.split():
-                if word not in self.vocabulary_:
-                    self.vocabulary_[word] = self.vocab_index_
-                    self.vocab_index_ += 1
+                if word in self.vocabulary_:
+                    self.vocabulary_[word] = self.vocabulary_[word] + 1
+                else:
+                    self.vocabulary_[word] = 1
+        for word, frequency in list(self.vocabulary_.items()):
+            if frequency < self.min_df:
+                del self.vocabulary_[word]
+        self.vocabulary_ = {
+            word: (i, frequency)
+            for i, (word, frequency) in enumerate(self.vocabulary_.items())
+        }
 
     def transform(self, documents):
         """
         Transform the text data into vectors.
         """
         vectors = []
-        for text in documents:
+        for document in documents:
             vector = np.zeros(len(self.vocabulary_), dtype=int)
-            for word in text.split():
+            for word in document.split():
                 if word in self.vocabulary_:
-                    vector[self.vocabulary_[word]] += 1
+                    vector[self.vocabulary_[word][0]] += 1
             vectors.append(vector)
         return np.array(vectors)
 
@@ -43,7 +51,4 @@ class CustomCountVectorizer:
         Fit and transform the text data into vectors.
         """
         self.fit(documents)
-        vectors = self.transform(documents)
-        frequency = np.sum(vectors, axis=0)
-        mask = frequency >= self.min_df
-        return vectors * mask
+        return self.transform(documents)
